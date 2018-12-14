a8ab13b357ab944f98c149afd0b551fbeae8c24e
vianney_victoria <vianney.victoria@serato.com>
2018-12-14 13:28:41 +1300

    start

---
+++ src/assets/data/seratocomponents.js
@@ -0,0 +1,55 @@
+const images = require.context('./images', true, /\.png$/);
+
+const seratocomponents = {
+  input: [
+    {
+      id: 1,
+      name: 'rca-input-left',
+      src: images('./rca-input-left.png'),
+    },
+    {
+      id: 2,
+      name: 'rca-input-right',
+      src: images('./rca-input-right.png'),
+    },
+    {
+      id: 3,
+      name: 'usb-input',
+      src: images('./usb-input.png'),
+    },
+    {
+      id: 4,
+      name: 'XLR-input',
+      src: images('./XLR-input.png'),
+    },
+    {
+      id: 5,
+      name: '1-8-input',
+      src: images('./1-8-input.png'),
+    },
+  ],
+  output: [
+    {
+      id: 6,
+      name: 'rca-output-left',
+      src: images('./rca-input-left.png'),
+    },
+    {
+      id: 7,
+      name: 'rca-output-right',
+      src: images('./rca-input-right.png'),
+    },
+    {
+      id: 8,
+      name: 'XLR-output',
+      src: images('./XLR-output.png'),
+    },
+    {
+      id: 9,
+      name: '1-8-input',
+      src: images('./1-8-input.png'),
+    },
+  ],
+};
+
+export default seratocomponents;
--- src/views/Serato.vue
+++ src/views/Serato.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>UX Tool to design Hardware</h1>
+  <div>
+    <h1>UX Tool to design Hardware</h1>
+    <SeratoLibrary />
+  </div>
 </template>
 
 
 <script>
+import SeratoLibrary from '@/components/SeratoLibrary.vue';
+
+export default {
+  name: 'serato',
+  components: {
+    SeratoLibrary,
+  },
+};
 
 </script>
 
 
 <style>
+
 
 </style>

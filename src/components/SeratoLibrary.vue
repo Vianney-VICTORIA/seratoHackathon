ddd78dae7fff63f400f2b48ef6051ed44466a69e
sherry.shahabzi <sshahbazi.nz@gmail.com>
2018-12-14 14:01:30 +1300

    Resolve problems

--- src/components/SeratoLibrary.vue
+++ src/components/SeratoLibrary.vue
@@ -1,22 +1,44 @@
 <template>
   <div class="wrapperComponent">
     <h2 v-bind:class="seratocomponents.input[0].name">Component Library</h2>
+
+    <div v-draggable="draggableValue">
+      <div :ref="handleId">
+        <img v-bin:src="seratocomponents.input[0].src" alt="move">
+      </div>
+      drag and drop using handler
+    </div>
+
   </div>
 </template>
 
-<script>
-
-import seratocomponents from '../assets/data/seratocomponents.js';
+<script type="text/ecmascript-6">
+import seratocomponents from '../assets/data/seratocomponents.js'
+import { Draggable } from 'draggable-vue-directive'
 
 export default {
   name: 'SeratoLibrary',
+  directives: {
+    Draggable
+  },
   data() {
     return {
       seratocomponents,
+      handleId: "handle-id",
+      draggableValue: {}
+    }
+  },
+  mounted() {
+    this.draggableValue.handle = this.$refs[this.handleId];
+    this.draggableValue.onPositionChange = this.onPosChanged;
+  },
+  methods: {
+    onPosChanged: function(pos) {
+      // console.log("left corner", pos.x);
+      // console.log("top corner", pos.y);
     }
   }
-};
-
+}
 </script>
 
 <style>
--- src/views/Serato.vue
+++ src/views/Serato.vue
@@ -1,45 +1,18 @@
 <template>
   <div id="serato">
     <h1>UX Tool to design Hardware</h1>
-
-    <div v-draggable="draggableValue">
-      <div :ref="handleId">
-        <img src="" alt="move">
-      </div>
-      drag and drop using handler
-    </div>
-
     <SeratoLibrary />
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  import SeratoLibrary from '@/components/SeratoLibrary.vue'
-  import { Draggable } from 'draggable-vue-directive'
-  export default {
-    components: {
-      SeratoLibrary,
-    },
-    directives: {
-      Draggable
-    },
-    data() {
-      return {
-        handleId: "handle-id",
-        draggableValue: {}
-      }
-    },
-    mounted() {
-      this.draggableValue.handle = this.$refs[this.handleId];
-      this.draggableValue.onPositionChange = this.onPosChanged;
-    },
-    methods: {
-      onPosChanged: function(pos) {
-        // console.log("left corner", pos.x);
-        // console.log("top corner", pos.y);
-      }
-    }
+import SeratoLibrary from '@/components/SeratoLibrary.vue'
+export default {
+  name: 'Serato',
+  components: {
+    SeratoLibrary,
   }
+}
 </script>
 
 <style>

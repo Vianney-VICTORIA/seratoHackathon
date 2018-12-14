e71717d8ce5757126001b41ac37276a147d82e02
sherry.shahabzi <sshahbazi.nz@gmail.com>
2018-12-14 13:04:42 +1300

    Add draggable vue directive and some sample code

--- src/views/Serato.vue
+++ src/views/Serato.vue
@@ -1,12 +1,41 @@
 <template>
-  <h1>UX Tool to design Hardware</h1>
+  <div id="serato">
+    <h1>UX Tool to design Hardware</h1>
+
+    <div v-draggable="draggableValue">
+      <div :ref="handleId">
+        <img src="" alt="move">
+      </div>
+      drag and drop using handler
+    </div>
+  </div>
 </template>
 
 
-<script>
-
+<script type="text/ecmascript-6">
+  import { Draggable } from 'draggable-vue-directive'
+  export default {
+    directives: {
+      Draggable
+    },
+    data() {
+      return {
+        handleId: "handle-id",
+        draggableValue: {}
+    }
+    },
+    mounted() {
+      this.draggableValue.handle = this.$refs[this.handleId];
+      this.draggableValue.onPositionChange = this.onPosChanged;
+    },
+    methods: {
+      onPosChanged: function(pos) {
+        // console.log("left corner", pos.x);
+        // console.log("top corner", pos.y);
+      }
+    }
+  }
 </script>
-
 
 <style>
 
